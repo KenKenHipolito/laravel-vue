8d1235776a3f2008808d71eb696be81889eac1c6
Kennedy Hipolito <khipolito@schools.ph>
2024-01-22 15:42:39 +0800

    Laravel sanctum api login

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -37,7 +37,7 @@ class TaskController extends Controller
 
     /**
      * Update the specified resource in storage.
-     */
+    */
     public function update(UpdateTaskRequest $request, Task $task)
     {
         $task->update($request->validated());
