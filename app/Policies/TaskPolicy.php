97e902881dfc8fe3fa003c2a3ae82e3802e1cd39
Kennedy Hipolito <khipolito@schools.ph>
2024-01-23 13:51:47 +0800

    Update policies

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -13,7 +13,7 @@ class TaskPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -22,14 +22,7 @@ class TaskPolicy
     public function view(User $user, Task $task): bool
     {
         //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
+        return $user->id === $task->user_id;
     }
 
     /**
@@ -38,6 +31,7 @@ class TaskPolicy
     public function update(User $user, Task $task): bool
     {
         //
+        return $user->id === $task->user_id;
     }
 
     /**
@@ -46,21 +40,6 @@ class TaskPolicy
     public function delete(User $user, Task $task): bool
     {
         //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Task $task): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Task $task): bool
-    {
-        //
+        return $user->id === $task->user_id;
     }
 }
